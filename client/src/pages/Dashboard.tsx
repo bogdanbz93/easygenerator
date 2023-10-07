8b3b086297f2e7d8568425a2729453b771bb4804
Bogdan <bogdanbz9933@gmail.com>
2023-10-07 18:35:20 +0300

    Small changes in design & routing

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,49 +4,49 @@ import { useAuth } from "../utils/AuthContext";
 
 // Default component
 const Dashboard: React.FC = () => {
-  const { logout } = useAuth();
+    const { logout } = useAuth();
 
-  return (
-    <div className="flex flex-col w-screen h-screen items-center justify-center gap-y-5 text-white p-5">
-      <img
-        src={Logo}
-        alt="Easy Generator Logo"
-        className="w-36 mb-5"
-        data-aos="fade-up"
-        data-aos-delay="250"
-        data-aos-duration="750"
-      />
-      <h1
-        className="text-2xl md:text-3xl"
-        data-aos="fade-up"
-        data-aos-delay="300"
-        data-aos-duration="750"
-      >
-        Welcome to the application
-      </h1>
-      <p
-        className="text-sm text-neutral-400 max-w-xs text-center"
-        data-aos="fade-up"
-        data-aos-delay="350"
-        data-aos-duration="750"
-      >
-        Thanks for viewing my application. If you want to resume the process,
-        click the button below.
-      </p>
-      <button
-        type="button"
-        onClick={async () => {
-          logout();
-        }}
-        className="button-primary"
-        data-aos="fade-up"
-        data-aos-delay="400"
-        data-aos-duration="750"
-      >
-        Log out
-      </button>
-    </div>
-  );
+    return (
+        <div className="flex flex-col w-screen h-screen items-center justify-center gap-y-5 text-white p-5">
+            <img
+                src={Logo}
+                alt="Easy Generator Logo"
+                className="w-36 mb-5"
+                data-aos="fade-up"
+                data-aos-delay="250"
+                data-aos-duration="750"
+            />
+            <h1
+                className="text-2xl md:text-3xl"
+                data-aos="fade-up"
+                data-aos-delay="300"
+                data-aos-duration="750"
+            >
+                Welcome to the application
+            </h1>
+            <p
+                className="text-sm text-neutral-400 max-w-xs text-center"
+                data-aos="fade-up"
+                data-aos-delay="350"
+                data-aos-duration="750"
+            >
+                Thanks for viewing my application. If you want to resume the
+                process, click the button below.
+            </p>
+            <button
+                type="button"
+                onClick={async () => {
+                    logout();
+                }}
+                className="button-primary"
+                data-aos="fade-up"
+                data-aos-delay="400"
+                data-aos-duration="750"
+            >
+                Log out
+            </button>
+        </div>
+    );
 };
 
 export default Dashboard;
--- client/src/utils/PrivateRoutes.tsx
+++ client/src/utils/PrivateRoutes.tsx
@@ -1,9 +1,8 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from "./AuthContext";
 
 const PrivateRoute = () => {
-  const { loggedIn } = useAuth();
-  return loggedIn ? <Outlet /> : <Navigate to="/login" />;
+    const loggedIn = localStorage.getItem("isLoggedIn");
+    return loggedIn ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
